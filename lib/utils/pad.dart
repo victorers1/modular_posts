15784137bb0717517fe7e07d80651ee4bc25dfd8
Victor Emanuel <victorers2@gmail.com>
2020-07-10 19:07:02 -0300

    feat(rand_int.dart): adds new util code
    rand method generates a random int in range [1, param] and applies zero padding

--- lib/utils/pad.dart
+++ lib/utils/pad.dart
@@ -1,4 +1,4 @@
-/// Addsa left 0 on integers less than 10
+/// Applies zero padding on integers less than 10
 /// When called on a integers greater than 9, returns it cast to String
 /// Extension method is available since Dart 2.7
 extension PadExtension on int {
