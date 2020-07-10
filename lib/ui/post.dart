3ac7cbca91b750c2ddf0b235c5cc0c0530c385a9
Victor Emanuel <victorers2@gmail.com>
2020-07-10 13:12:32 -0300

    feat(post): improves post widget

--- lib/ui/post.dart
+++ lib/ui/post.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:modular_posts/ui/post_footer.dart';
+import 'package:modular_posts/utils/capitalize.dart';
 
 class Post extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: CircleAvatar(
-              radius: 25,
+      child: Container(
+        padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: CircleAvatar(
+                radius: 25,
+              ),
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start, // TODO: test stretch
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('Title'),
-              Text('body'),
-              PostFooter(),
-            ],
-          )
-        ],
+            Expanded(
+              child: Column(
+                mainAxisAlignment:
+                    MainAxisAlignment.start, // TODO: test stretch
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+                          .capitalize(),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                  ),
+                  Text(
+                    'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+                        .capitalize(),
+                    maxLines: 15,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                  PostFooter(),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
