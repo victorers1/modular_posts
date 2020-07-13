d89777a0e715ae75ece6749d03cc9fa3a1dca567
Victor Emanuel <victorers2@gmail.com>
2020-07-13 10:38:41 -0300

    feat(profile): finishes profile tab
    adds sliverlist belowsliver app bar containing: username, street, city, zipcode, phone, company
    name, catch phrase and logout button

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -10,7 +10,7 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
-        future: userCtrl.getUser(1), // You are user 1
+        future: userCtrl.getUser(4), // You are user 4
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
@@ -49,12 +49,51 @@ class Profile extends StatelessWidget {
                     ),
                     SliverList(
                       delegate: SliverChildListDelegate(<Widget>[
-                        Container(height: 200, color: Colors.green),
-                        Container(height: 200, color: Colors.orange),
-                        Container(height: 200, color: Colors.black),
-                        Container(height: 200, color: Colors.red),
-                        Container(height: 200, color: Colors.orange),
-                        Container(height: 200, color: Colors.yellow),
+                        SizedBox(height: 10),
+                        ListTile(
+                          leading: Icon(Icons.face),
+                          title: Text('Username'),
+                          subtitle: Text(userCtrl.user.username),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.streetview),
+                          title: Text('Street'),
+                          subtitle: Text(userCtrl.user.address.street),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.pin_drop),
+                          title: Text('City'),
+                          subtitle: Text(userCtrl.user.address.city),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.location_city),
+                          title: Text('Zipcode'),
+                          subtitle: Text(userCtrl.user.address.city),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.phone),
+                          title: Text('Phone'),
+                          subtitle: Text(userCtrl.user.phone),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.web),
+                          title: Text('Website'),
+                          subtitle: Text(userCtrl.user.website),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.business_center),
+                          title: Text('Company'),
+                          subtitle: Text(userCtrl.user.company.name),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.insert_emoticon),
+                          title: Text('Catch phrase'),
+                          subtitle: Text(userCtrl.user.company.catchPhrase),
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.power_settings_new),
+                          title: Text('Logout'),
+                        ),
                       ]),
                     ),
                   ],
